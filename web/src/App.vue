caa164c565d377235715ad1d20865d32bc282c96
kernelai <redtomato158@gmail.com>
2025-01-08 20:42:03 +0800

    feat:add login

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,101 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-container>
+    <el-header>
+      <el-menu mode="horizontal" :ellipsis="false">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2">关于</el-menu-item>
+        <div class="flex-grow" />
+        <el-menu-item index="3">
+          <el-button type="primary">登录</el-button>
+        </el-menu-item>
+      </el-menu>
+    </el-header>
+
+    <el-main>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-card>
+            <template #header>
+              <div class="card-header">
+                <span>卡片标题</span>
+                <el-button class="button" text>操作按钮</el-button>
+              </div>
+            </template>
+            <div v-for="i in 4" :key="i" class="text item">
+              {{ '列表项 ' + i }}
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="12">
+          <el-form :model="form" label-width="120px">
+            <el-form-item label="活动名称">
+              <el-input v-model="form.name" />
+            </el-form-item>
+            <el-form-item label="活动时间">
+              <el-date-picker
+                v-model="form.date"
+                type="date"
+                placeholder="选择日期"
+                style="width: 100%"
+              />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">创建</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-main>
+
+    <el-footer>
+      <p>© 2025 我的应用</p>
+    </el-footer>
+  </el-container>
 </template>
 
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const form = reactive({
+  name: '',
+  date: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!', form)
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.el-container {
+  min-height: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.el-header {
+  padding: 0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+.el-footer {
+  text-align: center;
+  padding: 20px;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
 }
 </style>
